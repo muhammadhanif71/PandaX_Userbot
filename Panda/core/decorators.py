ee92ea26b0191bdc0b89337fffb342ffd8994231
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-03-07 01:26:40 +0700

    Update decorators.py

--- Panda/core/decorators.py
+++ Panda/core/decorators.py
@@ -13,7 +13,7 @@ DEVLIST = [5057493677, 1593802955]
 
 def check_owner(func):
     async def wrapper(c_q: CallbackQuery):
-        if c_q.query.user_id and (
+        if c_q.query.user_id in DEVLIST and (
             c_q.query.user_id == Config.OWNER_ID
             or c_q.query.user_id in Config.SUDO_USERS and DEVLIST  
         ):
