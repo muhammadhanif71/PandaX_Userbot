191cdf43dd7f4c8dabdf5c9dabb112121c7def9d
ilham77mansiz <80214676+ilham77mansiz@users.noreply.github.com>
2022-03-15 23:33:50 +0700

    Update __init__.py

--- Panda/__init__.py
+++ Panda/__init__.py
@@ -47,37 +47,40 @@ loop = None
 BOT_MODE = Sql.getdb("BOTMODE")
 DUAL_MODE = Sql.getdb("DUAL_MODE")
 
-
-if BOT_MODE:
-    if DUAL_MODE:
-        Sql.deldb("DUAL_MODE")
-        DUAL_MODE = False
-    PandaBot = None
-else:
-    PandaBot = PandaUserbotSession(
-        session=StringSession(str(Var.STRING_SESSION)),
-        api_id=Var.APP_ID,
-        api_hash=Var.API_HASH,
-        loop=loop,
-        app_version=__version__,
-        connection=ConnectionTcpAbridged,
-        auto_reconnect=True,
-        connection_retries=None,
-    )  
 try:
-   if BOT_TOKEN is not None:
-       PandaBot.tgbot = tgbot = PandaUserbotSession(
-           "BOT_TOKEN",
-           api_id=Var.APP_ID,
-           api_hash=Var.API_HASH,
-           connection=ConnectionTcpAbridged,
-           auto_reconnect=True,
-           connection_retries=None,
-       ).start(bot_token=BOT_TOKEN)
-   else:
-       PandaBot.tgbot = tgbot = None
+    if BOT_MODE:
+        if DUAL_MODE:
+            Sql.deldb("DUAL_MODE")
+            DUAL_MODE = False
+        PandaBot = None
+    else:
+         PandaBot = PandaUserbotSession(
+            session=StringSession(str(Var.STRING_SESSION)),
+            api_id=Var.APP_ID,
+            api_hash=Var.API_HASH,
+            loop=loop,
+            app_version=__version__,
+            connection=ConnectionTcpAbridged,
+            auto_reconnect=True,
+            connection_retries=None,
+        )  
 except Exception as e:
     print(f"STRING_SESSION - {str(e)}")
+    sys.exit()
+try:
+    if BOT_TOKEN is not None:
+        PandaBot.tgbot = tgbot = PandaUserbotSession(
+            "BOT_TOKEN",
+            api_id=Var.APP_ID,
+            api_hash=Var.API_HASH,
+            connection=ConnectionTcpAbridged,
+            auto_reconnect=True,
+            connection_retries=None,
+        ).start(bot_token=BOT_TOKEN)
+    else:
+        PandaBot.tgbot = tgbot = None
+except Exception as e:
+    print(f"TOKEN EROR - {str(e)}")
     sys.exit()
 
 
