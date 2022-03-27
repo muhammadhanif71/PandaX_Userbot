2badd1c600f41778bc5e73a0ffd408199a2dba78
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-03-27 09:31:12 +0700

    Update test.py

--- Panda/core/test.py
+++ Panda/core/test.py
@@ -3,19 +3,40 @@
 # Recode by Ilham Mansiz
 # ••••••••••••••••••••••√•••••••••••••√√√••••••••
 
+import asyncio
+import datetime
 import inspect
 import re
 import sys
+import traceback
 from pathlib import Path
 from typing import Dict, List, Union
 
+from telethon import TelegramClient, events
+from telethon.errors import (
+    AlreadyInConversationError,
+    BotInlineDisabledError,
+    BotResponseTimeoutError,
+    ChatSendInlineForbiddenError,
+    ChatSendMediaForbiddenError,
+    ChatSendStickersForbiddenError,
+    FloodWaitError,
+    MessageIdInvalidError,
+    MessageNotModifiedError,
+)
 from ..Config import Config
-from .. import PandaBot
+from ..helpers.utils.events import checking
+from ..helpers.utils.format import paste_text
+from ..helpers.utils.utils import runcmd
 from ..sql_helper import sqldb as SqL
 from . import BOT_INFO, CMD_INFO, GRP_INFO, LOADED_CMDS, PLG_INFO
 from .cmdinfo import _format_about
-from .data import _sudousers_list, blacklist_chats_list, sudo_enabled_cmds
+from .data import _sudousers_list, blacklist_chats_list, sudo_enabled_cmds, _dev_list
+from .events import MessageEdited, NewMessage
+from .fasttelethon import download_file, upload_file
 from .logger import logging
+from .managers import edit_delete
+from .pluginManager import restart_script
 
 LOGS = logging.getLogger(__name__)
 
@@ -52,6 +73,7 @@ sudo_enabledcmds = sudo_enabled_cmds()
 
 
 
+
 def ilhammansiz_cmd(
     pattern: str or tuple = None,
     info: Union[str, Dict[str, Union[str, List[str], Dict[str, str]]]]
@@ -59,26 +81,23 @@ def ilhammansiz_cmd(
     groups_only: bool = False,
     private_only: bool = False,
     allow_sudo: bool = True,
-    disable_edited: bool = False,
+    dev: bool = True,
+    dual: bool = False,
+    edited: bool = True,
     forword=False,
-    command: str = None,
-    **args,
-):
-    args["func"] = lambda e: e.via_bot_id is None
+    disable_errors: bool = False,
+    command: str or tuple = None,
+    **kwargs,
+) -> callable:  # sourcery no-metrics
+    kwargs["func"] = kwargs.get("func", lambda e: e.via_bot_id is None)
+    kwargs.setdefault("forwards", forword)
+    if SqL.getdb("blacklist_chats") is not None:
+        kwargs["blacklist_chats"] = True
+        kwargs["chats"] = blacklist_chats_list()
     stack = inspect.stack()
     previous_stack_frame = stack[1]
     file_test = Path(previous_stack_frame.filename)
     file_test = file_test.stem.replace(".py", "")
-
-    if "disable_edited" in args:
-        del args["disable_edited"]
-
-    args.setdefault("forwards", forword)
-
-    if SqL.getdb("blacklist_chats") is not None:
-        args["blacklist_chats"] = True
-        args["chats"] = blacklist_chats_list()
-    
     if command is not None:
         command = list(command)
         if not command[1] in BOT_INFO:
@@ -96,63 +115,206 @@ def ilhammansiz_cmd(
         if not command[0] in CMD_INFO:
             CMD_INFO[command[0]] = [_format_about(info)]
     if pattern is not None:
-        global panda
-        global sudo
         if (
             pattern.startswith(r"\#")
             or not pattern.startswith(r"\#")
             and pattern.startswith(r"^")
         ):
-            panda = sudo = re.compile(pattern)
+            REGEX_.regex1 = REGEX_.regex2 = re.compile(pattern)
         else:
-            panda_ = "\\" + Config.COMMAND_HAND_LER
-            sudo_ = "\\" + Config.SUDO_COMMAND_HAND_LER
-            panda = re.compile(panda_ + pattern)
-            sudo = re.compile(sudo_ + pattern)
-            if command is not None:
-                cmd1 = panda_ + command
-                cmd2 = sudo_ + command
-            else:
-                cmd1 = (
-                    (panda_ + pattern).replace("$", "").replace("\\", "").replace("^", "")
+            reg1 = "\\" + Config.COMMAND_HAND_LER
+            reg2 = "\\" + Config.SUDO_COMMAND_HAND_LER
+            devv = "\\" + Config.DEVS
+            duall = "\\" + dual_duall()
+            REGEX_.regex1 = re.compile(reg1 + pattern)
+            REGEX_.regex2 = re.compile(reg2 + pattern)
+            REGEX_.dev = re.compile(devv + pattern)
+            REGEX_.dual = re.compile(duall + pattern)
+
+
+
+    def decorator(func):  # sourcery no-metrics
+        async def wrapper(check):
+            if groups_only and not check.is_group:
+                return await edit_delete(
+                    check, "`I don't think this is a group.`", 10
                 )
-                cmd2 = (
-                    (sudo_ + pattern)
-                    .replace("$", "")
-                    .replace("\\", "")
-                    .replace("^", "")
+            if private_only and not check.is_private:
+               return await edit_delete(
+                    check, "`I don't think this is a personal Chat.`", 10
                 )
             try:
-                CMD_LIST[file_test].append(cmd1)
-            except BaseException:
-                CMD_LIST.update({file_test: [cmd1]})
+                await func(check)
+            except events.StopPropagation:
+                raise events.StopPropagation
+            except KeyboardInterrupt:
+                pass
+            except MessageNotModifiedError:
+                LOGS.error("Message was same as previous message")
+            except MessageIdInvalidError:
+                LOGS.error("Message was deleted or cant be found")
+            except BotInlineDisabledError:
+                await edit_delete(check, "`Turn on Inline mode for our bot`", 10)
+            except ChatSendStickersForbiddenError:
+                await edit_delete(
+                    check, "`I guess i can't send stickers in this chat`", 10
+                )
+            except BotResponseTimeoutError:
+                await edit_delete(
+                    check, "`The bot didnt answer to your query in time`", 10
+                )
+            except ChatSendMediaForbiddenError:
+                await edit_delete(check, "`You can't send media in this chat`", 10)
+            except AlreadyInConversationError:
+                await edit_delete(
+                    check,
+                    "`A conversation is already happening with the given chat. try again after some time.`",
+                    10,
+                )
+            except ChatSendInlineForbiddenError:
+                await edit_delete(
+                    check, "`You can't send inline messages in this chat.`", 10
+                )
+            except FloodWaitError as e:
+                LOGS.error(
+                    f"A flood wait of {e.seconds} occured. wait for {e.seconds} seconds and try"
+                )
+                await check.delete()
+                await asyncio.sleep(e.seconds + 5)
+            except BaseException as e:
+                LOGS.exception(e)
+                if not disable_errors:
+                    if Config.PRIVATE_GROUP_BOT_API_ID == 0:
+                        return
+                    date = (datetime.datetime.now()).strftime("%m/%d/%Y, %H:%M:%S")
+                    ftext = f"\nDisclaimer:\nFile ini hanya disisipkan di sini HANYA di sini,\
+                              \nkami hanya mencatat fakta kesalahan dan tanggal,\nKami menghormati privasi anda,\
+                              \nyou may not report this error if you've\
+                              \nany confidential data here, no one will see your data\
+                              \n\n--------BEGIN USERBOT TRACEBACK LOG--------\
+                              \nDate: {date}\nGroup ID: {str(check.chat_id)}\
+                              \nSender ID: {str(check.sender_id)}\
+                              \n\nEvent Trigger:\n{str(check.text)}\
+                              \n\nTraceback info:\n{str(traceback.format_exc())}\
+                              \n\nError text:\n{str(sys.exc_info()[1])}"
+                    new = {
+                        "error": str(sys.exc_info()[1]),
+                        "date": datetime.datetime.now(),
+                    }
+                    ftext += "\n\n--------END USERBOT TRACEBACK LOG--------"
+                    command = 'git log --pretty=format:"%an: %s" -5'
+                    ftext += "\n\n\nLast 5 commits:\n"
+                    output = (await runcmd(command))[:2]
+                    result = output[0] + output[1]
+                    ftext += result
+                    pastelink = paste_text(ftext)
+                    text = "**PandaUserbot Error report**\n\n"
+                    link = "[Klik](https://t.me/TEAMSquadUserbotSupport)"
+                    text += "If you wanna you can report it"
+                    text += f"- just forward this message {link}.\n"
+                    text += (
+                        "Nothing is logged except the fact of error and date\n\n"
+                    )
+                    text += f"**Error report : ** [{new['error']}]({pastelink})"
+                    await check.client.send_message(
+                        Config.PRIVATE_GROUP_BOT_API_ID, text, link_preview=False
+                    )
 
-    def decorator(func):
-        if not disable_edited:
-            PandaBot.add_event_handler(
-                func, events.MessageEdited(**args, outgoing=True, pattern=panda)
-            )
-        PandaBot.add_event_handler(
-            func, events.NewMessage(**args, outgoing=True, pattern=panda)
-        )
-        if allow_sudo:
-            if not disable_edited:
+        from .session import PandaBot
+          
+        if not func.__doc__ is None:
+            CMD_INFO[command[0]].append((func.__doc__).strip())
+        if pattern is not None:
+            if command is not None:
+                if command[0] in LOADED_CMDS and wrapper in LOADED_CMDS[command[0]]:
+                    return None
+                try:
+                    LOADED_CMDS[command[0]].append(wrapper)
+                except BaseException:
+                    LOADED_CMDS.update({command[0]: [wrapper]})
+            if edited:
                 PandaBot.add_event_handler(
-                    func,
-                    events.MessageEdited(
-                        **args, from_users=_sudousers_list(), pattern=sudo
-                    ),
+                    wrapper,
+                    MessageEdited(pattern=REGEX_.regex1, outgoing=True, **kwargs),
                 )
             PandaBot.add_event_handler(
-                func,
-                events.NewMessage(
-                    **args, from_users=_sudousers_list(), pattern=sudo
-                ),
+                wrapper,
+                NewMessage(pattern=REGEX_.regex1, outgoing=True, **kwargs),
             )
-        try:
-            LOADED_CMDS[file_test].append(func)
-        except Exception:
-            LOADED_CMDS.update({file_test: [func]})
-        return func
+            if dual and SqL.getdb("MODE_DUAL"):
+                PandaBot.tgbot.add_event_handler(
+                    wrapper,
+                    MessageEdited(pattern=REGEX_.dual, outgoing=True, **kwargs),
+                )
+            PandaBot.tgbot.add_event_handler(
+                wrapper,
+                NewMessage(pattern=REGEX_.dual, outgoing=True, **kwargs),
+            )
+            if dev is not None:
+                if command is not None or command[0]:
+                    if edited:
+                        PandaBot.add_event_handler(
+                            wrapper,
+                            MessageEdited(
+                                pattern=REGEX_.dev,
+                                from_users=_dev_list() or DEV,
+                                **kwargs,
+                            ),
+                        )
+                    PandaBot.add_event_handler(
+                        wrapper,
+                        NewMessage(
+                            pattern=REGEX_.dev,
+                            from_users=_dev_list() or DEV,
+                            **kwargs,
+                        ),
+                    )
+                    if dual and SqL.getdb("MODE_DUAL"):
+                        PandaBot.tgbot.add_event_handler(
+                            wrapper,
+                            MessageEdited(
+                                pattern=REGEX_.dual,
+                                from_users=_dev_list() or DEV,
+                                **kwargs,
+                            ),
+                        )
+                    PandaBot.tgbot.add_event_handler(
+                        wrapper,
+                        NewMessage(
+                            pattern=REGEX_.dual,
+                            from_users=_dev_list() or DEV,
+                            **kwargs,
+                        ),
+                    )
+            if allow_sudo and SqL.getdb("sudoenable") is not None:
+                if command is None or command[0] in sudo_enabledcmds:
+                    if edited:
+                        PandaBot.add_event_handler(
+                            wrapper,
+                            MessageEdited(
+                                pattern=REGEX_.regex2,
+                                from_users=_sudousers_list(),
+                                **kwargs,
+                            ),
+                        )
+                    PandaBot.add_event_handler(
+                        wrapper,
+                        NewMessage(
+                            pattern=REGEX_.regex2,
+                            from_users=_sudousers_list(),
+                            **kwargs,
+                        ),
+                    )
+        else:
+            if file_test in LOADED_CMDS and func in LOADED_CMDS[file_test]:
+                return None
+            try:
+                LOADED_CMDS[file_test].append(func)
+            except BaseException:
+               LOADED_CMDS.update({file_test: [func]})
+            if edited:
+                PandaBot.add_event_handler(func, events.MessageEdited(**kwargs))
+            PandaBot.add_event_handler(func, events.NewMessage(**kwargs))
+        return wrapper
 
     return decorator
