a252a4203e73514542189c0b1d828fb5d2b89bc7
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-07-16 23:24:57 +0700

    Update __init__.py

--- Panda/__init__.py
+++ Panda/__init__.py
@@ -73,22 +73,29 @@ try:
                 auto_reconnect=True,
                 connection_retries=None,
             )
-        if Var.STRING_SESSION2:
-             PandaBot2 = PandaUserbotSession(
-                session=StringSession(str(Var.STRING_SESSION2)),
-                api_id=Var.APP_ID,
-                api_hash=Var.API_HASH,
-                loop=loop,
-                app_version=__version__,
-                connection=ConnectionTcpAbridged,
-                auto_reconnect=True,
-                connection_retries=None,
-            )
 except Exception as e:
     print(f"STRING_SESSION {str(e)}")
     sys.exit()
 ######################################
 
+
+
+try:
+    if Var.STRING_SESSION2:
+        PandaBot2 = PandaUserbotSession(
+           session=StringSession(str(Var.STRING_SESSION2)),
+           api_id=Var.APP_ID,
+           api_hash=Var.API_HASH,
+           loop=loop,
+           app_version=__version__,
+           connection=ConnectionTcpAbridged,
+           auto_reconnect=True,
+           connection_retries=None,
+       )
+except Exception as e:
+    print(f"TOKEN- {str(e)}")
+    sys.exit()
+#########
 
 def ModeUserbot2():
     if Var.STRING_SESSION2:
