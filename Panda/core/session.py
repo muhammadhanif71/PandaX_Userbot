2c690187449dc3086406767e24b8988e250c1bf2
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-07-17 12:08:47 +0700

    Update session.py

--- Panda/core/session.py
+++ Panda/core/session.py
@@ -2,9 +2,10 @@
 ## Panda Userbot
 
 
-from Panda import PandaBot, PandaBot2
+from Panda import PandaBot, PandaBot2, PandaBot3
 Bot = PandaBot
 Stark = PandaBot
 tgbot = PandaBot.tgbot
 pandaub = PandaBot
 PandaBiT = PandaBot2
+boy = PandaBot3
