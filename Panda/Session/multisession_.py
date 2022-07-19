308908625257b6fde196ffc9257ec1c1f54271f4
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-07-19 10:07:16 +0700

    Update multisession_.py

--- Panda/Session/multisession_.py
+++ Panda/Session/multisession_.py
@@ -4,8 +4,8 @@ from .pyroclient import pyrotgbot
 def Pyrogram():
     if pyrotgbot:
         pyrotgbot.start()
-        pyrotgbot.me = await pyrotgbot.get_me()
+        pyrotgbot.me = pyrotgbot.get_me()
    
     if pyrobot:
        pyrobot.start()
-       pyrobot.me = await pyrobot.get_me()
+       pyrobot.me = pyrobot.get_me()
