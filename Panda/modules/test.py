8ea5a20f723681741881bab37760da21b6d29dcb
ilham77mansiz <imansiez7@gmail.com>
2022-07-19 03:13:14 +0000

    PandaX_Userbot: auto fixes
    Signed-off-by: ilham77mansiz <imansiez7@gmail.com>

--- Panda/modules/test.py
+++ Panda/modules/test.py
@@ -5,7 +5,7 @@ from pyrogram import filters
 
 @pyrotgbot.on_message(filters.command(["alive"]) & filters.incoming)
 async def alive(client, message):
-    lol = client.me
+    client.me
     await message.reply(
         f"`Yo ! {message.from_user.first_name} , I am Alive. Need Help ? How Are You? 🤟`"
     )
