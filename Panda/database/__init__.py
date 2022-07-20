65c9824271c5dafbc8130c90eff0a8d8e60e7d1d
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-07-20 10:01:21 +0700

    Create __init__.py

---
+++ Panda/database/__init__.py
@@ -0,0 +1,9 @@
+import motor.motor_asyncio
+from ..Var import Database
+
+
+def mongodb():
+    if Database.MONGO_DB:
+        return motor.motor_asyncio.AsyncIOMotorClient(Database.MONGO_DB)
+    else:
+        return None
