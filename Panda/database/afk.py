eb43611faae422bd8c08a6181f199c8ed94dfbf3
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-07-20 11:02:17 +0700

    Update afk.py

--- Panda/database/afk.py
+++ Panda/database/afk.py
@@ -1,25 +1,25 @@
-from . import db_x
+from .. import SqL
 
 
-afk = db_x["I_AFK"]
+afk = SqL
 
 
 async def go_afk(time, reason=""):
-    midhun = await afk.find_one({"_id": "AFK"})
+    midhun = afk.getdb({"_id": "AFK"})
     if midhun:
-        await afk.update_one({"_id": "AFK"}, {"$set": {"time": time, "reason": reason}})
+        await afk.setdb({"_id": "AFK"}, {"$set": {"time": time, "reason": reason}})
     else:
-        await afk.insert_one({"_id": "AFK", "time": time, "reason": reason})
+        await afk.setdb({"_id": "AFK", "time": time, "reason": reason})
 
 
 async def no_afk():
-    midhun = await afk.find_one({"_id": "AFK"})
+    midhun = afk.getdb({"_id": "AFK"})
     if midhun:
-        await afk.delete_one({"_id": "AFK"})
+        afk.deldb({"_id": "AFK"})
 
 
 async def check_afk():
-    midhun = await afk.find_one({"_id": "AFK"})
+    midhun = afk.getdb({"_id": "AFK"})
     if midhun:
         return midhun
     else:
