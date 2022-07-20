947211298368f5878701205aa83d24f50bca9041
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-07-20 09:47:24 +0700

    Update multisession_.py

--- Panda/Session/multisession_.py
+++ Panda/Session/multisession_.py
@@ -1,11 +1,120 @@
 
 from .pyroclient import pyrotgbot, pyrobot
+from telethon import functions, types, utils
 
-def Pyrogram():
+import logging
+from ..Var import Var, Database
+from logging import getLogger
+import pyrogram as pandapyro
+from .client import PandaBot, PandaBot2, PandaBot3
+from .._func.startup import load_modulesPyro, plugin_collecter
+from .pyroclient import pyrobot, pyrobot2, pyrobot3, pyrobot4, pyrotgbot
+import sys
+LOGS = getLogger(__name__)
+
+
+def Telethon():
+    if Var.STRING_SESSION:
+        try:
+            await PandaBot.start()
+            delta = await PandaBot(functions.help.GetConfigRequest())
+            for option in delta.dc_options:
+                if option.ip_address == PandaBot.session.server_address:
+                    if PandaBot.session.dc_id != option.id:
+                        LOGS.warning(
+                            f"Fixed DC ID in session from {PandaBot.session.dc_id}"
+                            f" to {option.id}"
+                        )
+                    PandaBot.session.set_dc(option.id, option.ip_address, option.port)
+                    PandaBot.session.save()
+                    break
+            PandaBot.me = await PandaBot.get_me()
+            bot_details = await PandaBot.tgbot.get_me()
+            PandaBot.uid = PandaBot.tgbot.uid = utils.get_peer_id(PandaBot.me)
+            if Var.OWNER_ID == 0:
+                Var.OWNER_ID = utils.get_peer_id(PandaBot.me)
+        except Exception as e:
+            LOGS.error(f"STRING_SESSION - {str(e)}")
+            sys.exit()
+
+
+    if Var.STRING_SESSION2:
+        try:
+            await PandaBot2.start()
+            delta = await PandaBot2(functions.help.GetConfigRequest())
+            for option in delta.dc_options:
+                if option.ip_address == PandaBot2.session.server_address:
+                    if PandaBot2.session.dc_id != option.id:
+                        LOGS.warning(
+                            f"Fixed DC ID in session from {PandaBot2.session.dc_id}"
+                            f" to {option.id}"
+                        )
+                    PandaBot2.session.set_dc(option.id, option.ip_address, option.port)
+                    PandaBot2.session.save()
+                    break
+            PandaBot2.me = await PandaBot2.get_me()
+            bot_details = await PandaBot.tgbot.get_me()
+            PandaBot.uid = PandaBot.tgbot.uid = utils.get_peer_id(PandaBot2.me)
+            if Var.OWNER_ID == 0:
+                Var.OWNER_ID = utils.get_peer_id(PandaBot2.me)
+        except Exception as e:
+            LOGS.error(f"STRING_SESSION - {str(e)}")
+            sys.exit()
+     
+    if Var.STRING_SESSION3:
+        try:
+            await PandaBot3.start()
+            delta = await PandaBot3(functions.help.GetConfigRequest())
+            for option in delta.dc_options:
+                if option.ip_address == PandaBot3.session.server_address:
+                    if PandaBot3.session.dc_id != option.id:
+                        LOGS.warning(
+                            f"Fixed DC ID in session from {PandaBot3.session.dc_id}"
+                            f" to {option.id}"
+                        )
+                    PandaBot3.session.set_dc(option.id, option.ip_address, option.port)
+                    PandaBot3.session.save()
+                    break
+            PandaBot3.me = await PandaBot3.get_me()
+            bot_details = await PandaBot.tgbot.get_me()
+            PandaBot.uid = PandaBot.tgbot.uid = utils.get_peer_id(PandaBot.me)
+            if Var.OWNER_ID == 0:
+                Var.OWNER_ID = utils.get_peer_id(PandaBot3.me)
+        except Exception as e:
+            LOGS.error(f"STRING_SESSION - {str(e)}")
+            sys.exit()
+
+
+async def Pyrogram():
     if pyrotgbot:
         pyrotgbot.start()
         pyrotgbot.me = pyrotgbot.get_me()
-   
+        assistant_mods = plugin_collecter("./assistant/")
+        for mods in assistant_mods:
+            try:
+                load_modulesPyro(mods, assistant=True)
+            except Exception as e:
+                logging.error("[ASSISTANT] - Failed To Load : " + f"{mods} - {str(e)}")
     if pyrobot:
-       pyrobot.start()
-       pyrobot.me = pyrobot.get_me()
+        pyrobot.start()
+        pyrobot.me = pyrobot.get_me()
+        pyrobot.has_a_bot = True if pyrotgbot else False
+    if pyrobot2:
+        pyrobot2.start()
+        pyrobot2.me = pyrobot2.get_me()
+        pyrobot2.has_a_bot = True if pyrotgbot else False
+    if pyrobot3:
+        pyrobot3.start()
+        pyrobot3.me = pyrobot3.get_me()
+        pyrobot3.has_a_bot = True if pyrotgbot else False
+    if pyrobot4:
+        pyrobot4.start()
+        pyrobot.me = pyrobot4.get_me()
+        pyrobot4.has_a_bot = True if pyrotgbot else False
+    needed_mods = plugin_collecter("./ModulesPyro/")
+    for nm in needed_mods:
+        try:
+            load_modulesPyro(nm)
+        except Exception as e:
+            logging.error("[USER] - Failed To Load : " + f"{nm} - {str(e)}")
+    pandapyro.idle()
