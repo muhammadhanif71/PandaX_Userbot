89e414ae7d4fba4c19c895a1c7e7d345589e4c2b
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-07-21 01:24:05 +0700

    Create gps.py

---
+++ ModulesPyro/gps.py
@@ -0,0 +1,41 @@
+import logging
+import os
+
+from geopy.geocoders import Nominatim
+
+from Panda._func.decorators import Panda_cmd as ilhammansiz_on_cmd
+from Panda._func._helpers import edit_or_reply, get_text
+
+GMAPS_LOC = "https://maps.googleapis.com/maps/api/geocode/json"
+
+
+@ilhammansiz_on_cmd(
+    ["gps"],
+    cmd_help={
+        "help": "Find and send the given location",
+        "example": "{ch}gps <text>",
+    },
+)
+async def gps(client, message):
+    pablo = await edit_or_reply(message, "`Processing...`")
+    args = get_text(message)
+    if not args:
+        await pablo.edit("Please Provide location for sending GPS")
+        return
+    try:
+        geolocator = Nominatim(user_agent="FridayUB")
+        location = args
+        geoloc = geolocator.geocode(location)
+        longitude = geoloc.longitude
+        latitude = geoloc.latitude
+    except Exception as e:
+        logging.info(e)
+        await pablo.edit("`I Can't Find That Location!`")
+        return
+    gm = "https://www.google.com/maps/search/{},{}".format(latitude, longitude)
+    await client.send_location(message.chat.id, float(latitude), float(longitude))
+    await pablo.reply(
+        "Open with: [Google Maps]({})".format(gm),
+        disable_web_page_preview=False,
+    )
+    await pablo.delete()
