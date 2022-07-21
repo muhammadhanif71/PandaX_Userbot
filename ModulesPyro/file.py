8bb7f82f681d621b05cf33772e7bdef2a72d31dc
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-07-21 22:44:59 +0700

    Update file.py

--- ModulesPyro/file.py
+++ ModulesPyro/file.py
@@ -8,7 +8,7 @@ import time
 import uuid
 
 import img2pdf
-from fsplit.filesplit import Filesplit
+
 
 from Panda._func.decorators import Panda_cmd as ilhammansiz_on_cmd
 from Panda._func._helpers import (
