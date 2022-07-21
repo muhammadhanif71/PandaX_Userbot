1b8f85ac09168dac32fb82fe9a9bca5b3ee7742d
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-07-22 00:42:45 +0700

    Add files via upload

---
+++ ModulesPyro/time_.py
@@ -0,0 +1,24 @@
+# 
+
+from Petercord_Userbot.core.decorators import ilhammansiz_on_cmd
+from Petercord_Userbot.helper_func.basic_helpers import edit_or_reply, get_text, edit_or_send_as_file
+import calendar
+from datetime import datetime
+
+
+@ilhammansiz_on_cmd(['time'],
+               cmd_help={
+                   "help": "Check Current Date , Time & Calender.",
+                   "example": "{ch}time"
+              }
+)
+async def _d(client, message):
+    year_ = datetime.now().year
+    date_ = datetime.now().day
+    month_ = datetime.now().month
+    mydate = datetime.now()
+    da = mydate.strftime("Date : %d \nMonth : %B \nYear : %Y")
+    dt = mydate.strftime("Hour : %H \nMinute : %M")
+    cal_ = calendar.month(year_, month_)
+    f_d = f"<code>{cal_}\n{da} \n\n{dt}</code>"
+    await edit_or_reply(message, f_d, parse_mode="html")
