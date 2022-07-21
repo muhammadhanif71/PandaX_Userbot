91209b886ba63605e6611a485b36f9a4c39968ff
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-07-22 01:14:51 +0700

    Update help.py

--- ModulesPyro/help.py
+++ ModulesPyro/help.py
@@ -2,7 +2,7 @@ from Panda import CMD_LIST, XTRA_CMD_LIST, pyrotgbot as bot
 from Panda._func.decorators import Config, Panda_cmd as ilhammansiz_on_cmd
 from Panda._func._helpers import edit_or_reply, get_text
 
-
+from Panda import config
 
 
 @ilhammansiz_on_cmd(
@@ -42,7 +42,7 @@ async def help(client, message):
     ["help", "ahelper"],
     cmd_help={
         "help": "Gets Help List & Info",
-        "example": "{ch}help (cmd_name)",
+        "example": "{ch}ahelp (cmd_name)",
     },
 )
 async def help_(client, message):
@@ -63,14 +63,18 @@ def get_help_str(string):
     if string not in CMD_LIST.keys():
         if string not in XTRA_CMD_LIST.keys():
             return None
-        return CMD_LIST[string]
-    return XTRA_CMD_LIST[string]
+        return XTRA_CMD_LIST[string]
+    return CMD_LIST[string]
     
 def prepare_cmd_list():
-    main_l = f"<b><u>🐼 PandaUserbot CommandList 🐼</b></u> \n\n<b>Main Command List ({len(CMD_LIST)}) :</b> \n\n"
+    main_l = f"<b><u> PandaUserbot CommandList</b></u> \n\n<b> Main CommandList ({len(CMD_LIST)}) :</b> \n\n"
     for i in CMD_LIST:
         if i:
-            main_l += "`" + str(i)
-    main_l += f"\n\nUse <code>{Config.COMMAND_HANDLER}help (cmd-name)</code> To Know More About A Plugin."
+            main_l += f"<code>{i}</code>    "
+    if Config.LOAD_UNOFFICIAL_PLUGINS:
+        main_l += f"\n\n<b> XtraCommand List ({len(XTRA_CMD_LIST)}) :</b> \n\n"
+        for i in XTRA_CMD_LIST:
+            if i:
+                main_l += f"<code>{i}</code>    "
+    main_l += f"\n\nUse <code>{Config.COMMAND_HANDLER}ahelp (cmd-name)</code> To Know More About A Plugin."
     return main_l 
-    
