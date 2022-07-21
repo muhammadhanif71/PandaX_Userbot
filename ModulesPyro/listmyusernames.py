85aa135bcde2265d6d84441d03abcc488b468a88
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-07-21 08:48:22 +0700

    Create listmyusernames.py

---
+++ ModulesPyro/listmyusernames.py
@@ -0,0 +1,28 @@
+import pyrogram
+
+from Panda._func.decorators import Panda_cmd as ilhammansiz_on_cmd
+from Panda._func._helpers import edit_or_reply, edit_or_send_as_file
+
+
+@ilhammansiz_on_cmd(
+    ["listmyusernames"],
+    cmd_help={
+        "help": "Get All Admin Channel / Chat List",
+        "example": "{ch}listmyusernames",
+    },
+)
+async def pabloescobar(client, message):
+    pablo = await edit_or_reply(message, "`Please Wait!`")
+    channels = await client.send(
+        pyrogram.raw.functions.channels.GetAdminedPublicChannels()
+    )
+    C = channels.chats
+    output_stre = ""
+    for x in C:
+        output_stre += f"{x.title}\n@{x.username}\n\n"
+    output_str = f"""I am Admin In All These Groups And Channels
+{output_stre}
+"""
+    await edit_or_send_as_file(
+        output_str, pablo, client, "Your Admin Chats", "admin_chat"
+    )
