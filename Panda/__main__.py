db63f6b48a305d88722ec4e847a05b6ab9ea3004
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-07-22 08:17:37 +0700

    Update __main__.py

--- Panda/__main__.py
+++ Panda/__main__.py
@@ -6,10 +6,8 @@
 
 import sys
 import Panda
-from Panda import utils
 LOGS = Panda.core.logger.logging.getLogger("PandaUserbot")
-from .utils import P, M, V, A
-from .Session.multisession_ import Pyrogram, Telethon
+from .Session.multisession_ import Pyrogram
 
 ## Memulai ••••••••••√√√√√•••••••
 
@@ -22,31 +20,6 @@ except Exception as e:
 
 ## Install Modules ••••••√√√√√••••••
 
-async def memulai():
-    await utils.loads(f"{P}")
-    await utils.loads(f"{M}")
-    await utils.buka(f"{V}")
-    await utils.buka(f"{A}")
-    
-
-def start():
-    if Panda.PandaBot:
-        Panda.PandaBot.loop.run_until_complete(memulai())
-        Panda.PandaBot.loop.run_until_complete(utils.join())
-        Panda.PandaBot.loop.run_until_complete(utils.ongrup())
-        LOGS.info(f"꧁༺ Panda Userbot ༻꧂\n⚙️ Version:{Panda.__version__} [TELAH DIAKTIFKAN]")
-
 if __name__ == "__main__":
-    Telethon()
-    start()
     Pyrogram()
   
-if Panda.PandaBot:
-    try:
-        if len(sys.argv) not in (1, 3, 4):
-            Panda.PandaBot.disconnect()
-        else:
-            Panda.PandaBot.run_until_disconnected()
-    except Exception as e:
-        LOGS.info(str(e), exc_info=True)
-        sys.exit(1)
