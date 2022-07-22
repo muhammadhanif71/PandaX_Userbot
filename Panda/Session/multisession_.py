660643462011fbbe82529a0a2fc5eb4805cc0612
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-07-22 08:18:48 +0700

    Update multisession_.py

--- Panda/Session/multisession_.py
+++ Panda/Session/multisession_.py
@@ -6,83 +6,11 @@ import logging
 from ..Var import Var
 from logging import getLogger
 import pyrogram as pandapyro
-from .client import PandaBot, PandaBot2, PandaBot3
 from .._func.startup import load_modulesPyro, plugin_collecter
 from .pyroclient import pyrobot, pyrobot2, pyrobot3, pyrobot4, pyrotgbot
 import sys
 LOGS = getLogger(__name__)
 
-
-def Telethon():
-    if Var.STRING_SESSION:
-        try:
-            PandaBot.start()
-            delta = PandaBot(functions.help.GetConfigRequest())
-            for option in delta.dc_options:
-                if option.ip_address == PandaBot.session.server_address:
-                    if PandaBot.session.dc_id != option.id:
-                        LOGS.warning(
-                            f"Fixed DC ID in session from {PandaBot.session.dc_id}"
-                            f" to {option.id}"
-                        )
-                    PandaBot.session.set_dc(option.id, option.ip_address, option.port)
-                    PandaBot.session.save()
-                    break
-            PandaBot.me = PandaBot.get_me()
-            PandaBot.tgbot.get_me()
-            PandaBot.uid = PandaBot.tgbot.uid = utils.get_peer_id(PandaBot.me)
-            if Var.OWNER_ID == 0:
-                Var.OWNER_ID = utils.get_peer_id(PandaBot.me)
-        except Exception as e:
-            LOGS.error(f"STRING_SESSION - {str(e)}")
-            sys.exit()
-
-
-    if Var.STRING_SESSION2:
-        try:
-            PandaBot2.start()
-            delta = PandaBot2(functions.help.GetConfigRequest())
-            for option in delta.dc_options:
-                if option.ip_address == PandaBot2.session.server_address:
-                    if PandaBot2.session.dc_id != option.id:
-                        LOGS.warning(
-                            f"Fixed DC ID in session from {PandaBot2.session.dc_id}"
-                            f" to {option.id}"
-                        )
-                    PandaBot2.session.set_dc(option.id, option.ip_address, option.port)
-                    PandaBot2.session.save()
-                    break
-            PandaBot2.me = PandaBot2.get_me()
-            PandaBot.tgbot.get_me()
-            PandaBot.uid = PandaBot.tgbot.uid = utils.get_peer_id(PandaBot2.me)
-            if Var.OWNER_ID == 0:
-                Var.OWNER_ID = utils.get_peer_id(PandaBot2.me)
-        except Exception as e:
-            LOGS.error(f"STRING_SESSION - {str(e)}")
-            sys.exit()
-     
-    if Var.STRING_SESSION3:
-        try:
-            PandaBot3.start()
-            delta = PandaBot3(functions.help.GetConfigRequest())
-            for option in delta.dc_options:
-                if option.ip_address == PandaBot3.session.server_address:
-                    if PandaBot3.session.dc_id != option.id:
-                        LOGS.warning(
-                            f"Fixed DC ID in session from {PandaBot3.session.dc_id}"
-                            f" to {option.id}"
-                        )
-                    PandaBot3.session.set_dc(option.id, option.ip_address, option.port)
-                    PandaBot3.session.save()
-                    break
-            PandaBot3.me = PandaBot3.get_me()
-            PandaBot.tgbot.get_me()
-            PandaBot.uid = PandaBot.tgbot.uid = utils.get_peer_id(PandaBot.me)
-            if Var.OWNER_ID == 0:
-                Var.OWNER_ID = utils.get_peer_id(PandaBot3.me)
-        except Exception as e:
-            LOGS.error(f"STRING_SESSION - {str(e)}")
-            sys.exit()
 
 
 def Pyrogram():
