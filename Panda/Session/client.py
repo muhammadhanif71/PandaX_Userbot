78b18ae8374e69a856e5256666cbfea964b63ab5
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-07-23 08:47:16 +0700

    Update client.py

--- Panda/Session/client.py
+++ Panda/Session/client.py
@@ -85,15 +85,14 @@ except Exception as e:
     sys.exit()
 #########
 
-try:
-    if Database.BOT_TOKEN:
-        PandaBot.tgbot = PandaUserbotSession(
-            "BOT_TOKEN",
-            api_id=Var.APP_ID,
-            api_hash=Var.API_HASH,
-        ).start(bot_token=Database.BOT_TOKEN)
-    else:
-        PandaBot.tgbot = None
-except Exception as e:
-    print(f"BOT_TOKEN- {str(e)}")
-    sys.exit()
+if Database.BOT_TOKEN is not None:
+    PandaBot.tgbot = PandaUserbotSession(
+        "TG_BOT_TOKEN",
+        api_id=Var.APP_ID,
+        api_hash=Var.API_HASH,
+        connection=ConnectionTcpAbridged,
+        auto_reconnect=True,
+        connection_retries=None,
+    ).start(bot_token=Database.BOT_TOKEN)
+else:
+    PandaBot.tgbot = None
