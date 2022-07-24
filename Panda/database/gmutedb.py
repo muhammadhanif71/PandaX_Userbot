461df49f783a03a9362e86fd491c66272b82e57f
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-07-24 22:54:11 +0700

    Update gmutedb.py

--- Panda/database/gmutedb.py
+++ Panda/database/gmutedb.py
@@ -3,18 +3,18 @@ from . import db_x
 gmuteh = db_x["GMUTE"]
 
 
-def is_gmuted(sender_id):
-    kk = gmuteh.find_one({"sender_id": sender_id})
+async def is_gmuted(sender_id):
+    kk = await gmuteh.find_one({"sender_id": sender_id})
     if not kk:
         return False
     else:
         return True
 
 
-def gmute(sender_id, reason="#GMuted"):
-    return gmuteh.insert_one({"sender_id": sender_id, "reason": reason})
+async def gmute(sender_id, reason="#GMuted"):
+    await gmuteh.insert_one({"sender_id": sender_id, "reason": reason})
 
 
-def ungmute(sender_id):
-    return gmuteh.delete_one({"sender_id": sender_id})
+async def ungmute(sender_id):
+    await gmuteh.delete_one({"sender_id": sender_id})
 
