1fbfe6083ce5faca74fe898b879a43b3ad2b1c8a
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-07-25 12:12:49 +0700

    Update __init__.py

--- Panda/database/__init__.py
+++ Panda/database/__init__.py
@@ -1,6 +1,7 @@
 import motor.motor_asyncio
 from ..Var import Database
 import logging
+from ..sql_helper.db import BaseDB
 """
 def mongodb():
     if Database.MONGO_DB:
@@ -13,52 +14,5 @@ db_x = mongodb()
 
 """
 
-__all__ = ['get_collection']
 
-import asyncio
-from typing import List
-
-from motor.motor_asyncio import AsyncIOMotorClient
-from motor.core import AgnosticClient, AgnosticDatabase, AgnosticCollection
-
-
-
-_LOG = logging.getLogger(__name__)
-_LOG_STR = "$$$>>> %s <<<$$$"
-
-
-_MGCLIENT: AgnosticClient = AsyncIOMotorClient(Database.MONGO_DB)
-_RUN = asyncio.get_event_loop().run_until_complete
-
-if "Panda" in _RUN(_MGCLIENT.list_database_names()):
-    _LOG.info(_LOG_STR, "Panda Database Found :) => Now Logging to it...")
-else:
-    _LOG.info(_LOG_STR, "Panda Database Not Found :( => Creating New Database...")
-
-_DATABASE: AgnosticDatabase = _MGCLIENT["Panda"]
-_COL_LIST: List[str] = _RUN(_DATABASE.list_collection_names())
-
-
-def get_collection(name: str) -> AgnosticCollection:
-    """ Create or Get Collection from your database """
-    if name in _COL_LIST:
-        _LOG.debug(_LOG_STR, f"{name} Collection Found :) => Now Logging to it...")
-    else:
-        _LOG.debug(_LOG_STR, f"{name} Collection Not Found :( => Creating New Collection...")
-    return _DATABASE[name]
-
-
-def _close_db() -> None:
-    _MGCLIENT.close()
-
-
-
-
-def mongodb():
-    if Database.MONGO_DB:
-        return get_collection
-    else:
-        return None
-
-
-db_x = mongodb()
+db_x = BaseDB()
