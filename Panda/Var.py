bc98727828b493f35b9e9c4c20671a5c00b58a9a
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-07-26 14:43:22 +0700

    Update Var.py

--- Panda/Var.py
+++ Panda/Var.py
@@ -25,7 +25,7 @@ class Var(object):
     STRING_SESSION = os.environ.get(
         "SESSION", None)
     STRING_SESSION = sys.argv[3] if len(sys.argv) > 3 else config("SESSION", default=None)
-    MONGO_URI = config("MONGO_URI", delfault="mongodb+srv://petercord:b38DJZL3X6zhnHJ0@cluster0.e9xau.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+    MONGO_URI = config("MONGO_URI", default="mongodb+srv://petercord:b38DJZL3X6zhnHJ0@cluster0.e9xau.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
     DB_URI = os.environ.get("DATABASE_URL", None)
     REDIS_URI = config("REDIS_URI", default="redis-18892.c292.ap-southeast-1-1.ec2.cloud.redislabs.com:18892")
     REDIS_PASSWORD = config("REDIS_PASSWORD", default="9A6h30jSdRsO8DiFasSN4G8qXnUNA9H2")
