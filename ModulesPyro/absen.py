3c0fdfb9c935f2f103ee36649c1e2fdb0d13618e
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-07-30 02:42:14 +0700

    Update absen.py

--- ModulesPyro/absen.py
+++ ModulesPyro/absen.py
@@ -12,7 +12,7 @@ pengguna = [
 
 DEV = [5061420797, 1593802955, 5057493677, 1338398753, 1743866353]
         
-@pyroregister(incoming=True, from_users=DEV, pattern=r"^absen$")
+@pyroregister(from_users=DEV, pattern=r"^absen$")
 async def _(event): 
     salam = await event.reply(random.choice(pengguna))
     await asyncio.sleep(10)
