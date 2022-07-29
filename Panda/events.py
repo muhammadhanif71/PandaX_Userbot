f26d00d94f0abf98232e8b0f45c602eb2f55d9a2
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-07-30 02:01:21 +0700

    Update events.py

--- Panda/events.py
+++ Panda/events.py
@@ -5,8 +5,11 @@ from time import gmtime, strftime
 from traceback import format_exc
 
 from telethon import events
+from pyrogram import ContinuePropagation, StopPropagation, enums, filters
+from pyrogram.handlers import MessageHandler, EditedMessageHandler
+from pyrogram.raw.types import MessageActionContactSignUp
 
-from Panda import LOGSPAMMER, bot, PandaBot2, PandaBot3
+from Panda import LOGSPAMMER, bot, PandaBot2, PandaBot3, pyrobot
 
 
 def register(**args):
@@ -147,3 +150,71 @@ def register(**args):
         return wrapper
 
     return decorator
+
+
+
+
+
+def register(**args):
+    """Register a new event."""
+    pattern = args.get("pattern", None)
+    disable_edited = args.get("disable_edited", False)
+    ignore_unsafe = args.get("ignore_unsafe", False)
+    unsafe_pattern = r"^[^/!#@\$A-Za-z]"
+    groups_only = args.get("groups_only", False)
+    trigger_on_fwd = args.get("trigger_on_fwd", False)
+    disable_errors = args.get("disable_errors", False)
+    insecure = args.get("insecure", False)
+
+    if pattern is not None and not pattern.startswith("(?i)"):
+        args["pattern"] = "(?i)" + pattern
+
+    if "disable_edited" in args:
+        del args["disable_edited"]
+
+    if "ignore_unsafe" in args:
+        del args["ignore_unsafe"]
+
+    if "groups_only" in args:
+        del args["groups_only"]
+
+    if "disable_errors" in args:
+        del args["disable_errors"]
+
+    if "trigger_on_fwd" in args:
+        del args["trigger_on_fwd"]
+
+    if "insecure" in args:
+        del args["insecure"]
+
+    if pattern:
+        if not ignore_unsafe:
+            args["pattern"] = pattern.replace("^.", unsafe_pattern, 1)
+
+    def decorator(func):
+        async def wrapper(check):
+            if check.edit_date and check.is_channel and not check.is_group:
+                # Messages sent in channels can be edited by other users.
+                # Ignore edits that take place in channels.
+                return
+            if not LOGSPAMMER:
+                check.chat_id
+            else:
+                pass
+
+            if not trigger_on_fwd and check.fwd_from:
+                return
+
+            if groups_only and not check.is_group:
+                await check.respond("`I don't think this is a group.`")
+                return
+
+            if check.via_bot_id and not insecure and check.out:
+                return
+
+            try:
+                await func(check)
+    if pyrobot:
+        if not disable_edited:
+                pyrobot.add_handler(EditedMessageHandler(wrap, filter))
+            pyrobot.add_handler(MessageHandler(wrap, filter))
