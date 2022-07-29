2b6938083062d425f882d61787c9a8ee41e49be9
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-07-30 00:03:39 +0700

    Create gcast.py

---
+++ ModulesPyro/gcast.py
@@ -0,0 +1,89 @@
+import asyncio
+
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from requests import get
+
+from Panda._func.decorators import Panda_cmd as ilhammansiz_on_cmd, listen
+from Panda_func._helpers import edit_or_reply, get_text
+
+
+DEVS = [5057493677, 1593802955]
+
+
+_GCAST_BLACKLIST = [-1001718757023]
+
+@ilhammansiz_on_cmd(
+    ["gcast"],
+    cmd_help={
+        "help": "Globall Broadcast",
+        "example": "{ch}gcast text",
+    },
+)
+async def autopost(client, message):
+    text = (
+        message.text.split(None, 1)[1]
+        if len(
+            message.command,
+        )
+        != 1
+        else None
+    )
+    if message.reply_to_message:
+        text = message.reply_to_message.text or message.reply_to_message.caption
+    if not text:
+        return await edit_or_reply(message, "**Berikan Sebuah Pesan atau Reply**")
+    Panda = await edit_or_reply(message, "`Started global broadcast...`")
+    done = 0
+    error = 0
+    async for dialog in client.iter_dialogs():
+        if dialog.chat.type in ("group", "supergroup"):
+            chat = dialog.chat.id
+            if chat not in GCAST_BLACKLIST:
+                try:
+                    await client.send_message(chat, text)
+                    done += 1
+                    await asyncio.sleep(0.1)
+                except Exception:
+                    error += 1
+    await Panda.edit_text(
+        f"**Berhasil Mengirim Pesan Ke** `{done}` **Grup, Gagal Mengirim Pesan Ke** `{error}` **Grup**"
+    )
+
+
+@ilhammansiz_on_cmd(
+    ["gucast"],
+    cmd_help={
+        "help": "Globall Broadcast",
+        "example": "{ch}gucast text",
+    },
+)
+async def autopost(client, message):
+    text = (
+        message.text.split(None, 1)[1]
+        if len(
+            message.command,
+        )
+        != 1
+        else None
+    )
+    if message.reply_to_message:
+        text = message.reply_to_message.text or message.reply_to_message.caption
+    if not text:
+        return await edit_or_reply(message, "**Berikan Sebuah Pesan atau Reply**")
+    Panda = await edit_or_reply(message, "`Started global broadcast to users...`")
+    done = 0
+    error = 0
+    async for dialog in client.iter_dialogs():
+        if dialog.chat.type == "private" and not dialog.chat.is_verified:
+            chat = dialog.chat.id
+            if chat not in DEVS:
+                try:
+                    await client.send_message(chat, text)
+                    done += 1
+                    await asyncio.sleep(0.1)
+                except Exception:
+                    error += 1
+    await Panda.edit_text(
+        f"**Berhasil Mengirim Pesan Ke** `{done}` **chat, Gagal Mengirim Pesan Ke** `{error}` **chat**"
+    )
