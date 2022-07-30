ba2c0cff2129ee405ff12c1a5219eaf79e011525
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-07-30 08:47:04 +0700

    Update absen.py

--- ModulesPyro/absen.py
+++ ModulesPyro/absen.py
@@ -2,6 +2,8 @@ import random
 from Panda.events import pyroregister
 import asyncio
 
+from pyrogram import filters
+
 pengguna = [
     f"Perkenalkan Nama saya Panda\nTerimah Kasih Ganteng 😏",
     f"Saya Panda Hadir Kang mas ucok butet neng atau apalah 😂😏",
@@ -12,7 +14,7 @@ pengguna = [
 
 DEV = [5061420797, 1593802955, 5057493677, 1338398753, 1743866353]
         
-@pyroregister(dev=True, pattern=r"^absen$")
+@pyroregister(dev=True, filters.user(DEV), pattern=r"^absen$")
 async def absen(client, message): 
     salam = await message.reply(random.choice(pengguna))
     await asyncio.sleep(10)
