4f93fc6481b29c3b99e1200e6ecec448984ce2b6
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-07-31 18:49:52 +0700

    Update __init__.py

--- Panda/Session/__init__.py
+++ Panda/Session/__init__.py
@@ -1,2 +1,7 @@
 from .client import *
 from .pyroclient import *
+
+
+Telethonbot = [Telethonbot for Telethonbot in [PandaBot, PandaBot2, PandaBot3] if Telethonbot]
+Pyrogrambot = [Pyrogrambot for Pyrogrambot in [pyrobot, pyrobot2, pyrobot3, pyrobot4] if Pyrogrambot]
+ 
