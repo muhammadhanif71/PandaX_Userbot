12aa4ad471c8202480569687ab28eda71f02b366
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-07-31 19:23:07 +0700

    Update __main__.py

--- Panda/__main__.py
+++ Panda/__main__.py
@@ -38,6 +38,8 @@ def start():
         Panda.PandaBot.loop.run_until_complete(utils.join())
         Panda.PandaBot.loop.run_until_complete(utils.ongrup())
         LOGS.info(f"꧁༺ Panda Userbot ༻꧂\n⚙️ Version:{Panda.__version__} [TELAH DIAKTIFKAN]")
+    if Panda.pyrobot:
+        pyrobot.PandaBot.loop.run_until_complete(utils.ongruppyro())
 
 
 
