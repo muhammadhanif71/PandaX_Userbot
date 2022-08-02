4dd7e77fea925d8c5b9619c12df4e5a7fec6cb26
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-08-02 08:03:58 +0700

    Update welcomedb.py

--- Panda/database/welcomedb.py
+++ Panda/database/welcomedb.py
@@ -6,7 +6,16 @@ def get_stuff(key=None):
 
 def add_welcome(chat_id, message_id):
     ok = get_stuff("WELCOME")
-    ok.update({chat: {{"chat_id": chat_id, "msg_id": message_id})
+    ok.update({"chat_id": chat_id, "msg_id": message_id})
     return SqL.setdb("WELCOME", ok)
 
 
+def del_welcome(chat_id):
+    ok = get_stuff("WELCOME")
+    if ok.get(chat_id):
+        ok.pop(chat_id)
+        return SqL.setdb("WELCOME", ok)
+
+def welcome_info(chat_id):
+    ok = get_stuff("WELCOME")
+    return ok.get(chat_id)
