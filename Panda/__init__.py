d37a5621d90ff55725d020ec88ab28aed4fdb24f
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-08-10 21:17:45 +0700

    Update __init__.py

--- Panda/__init__.py
+++ Panda/__init__.py
@@ -52,8 +52,8 @@ __copyright__ = __copyright__
 LOGS = logging.getLogger("PandaUserbot")
 loop = None
 
-LOGS.info(f"Connecting to {SqL.name}...")
-LOGS.info(f"Connected to {SqL.name} Successfully!")
+LOGS.info(f"Memeriksa {SqL.name}...")
+LOGS.info(f"Terkoneksi {SqL.name} Successfully!")
 
 
 BOT_MODE = SqL.getdb("MODE_DUAL")
