91a4339a12a997d8512a4cd217554bf4a0391f22
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-08-12 08:22:08 +0700

    Create joingrup.py

---
+++ ModulesPyro/joingrup.py
@@ -0,0 +1,68 @@
+from pyrogram import Client, filters
+from pyrogram.types import Message
+
+from Panda._func.decorators import Panda_cmd as ilhammansiz_on_cmd, listen
+from Panda._func._helpers import edit_or_reply, get_text
+from . import HELP
+
+
+HELP(
+    "joingrup",
+)
+
+@ilhammansiz_on_cmd(
+    ["joingrup"],
+    cmd_help={
+        "help": "Untuk bergabung ke obrolan dengan username gc",
+        "example": "{ch}joingrup @PandaUserbot",
+    },
+)
+async def join(client, message):
+    panda = message.command[1] if len(message.command) > 1 else message.chat.id
+    xxnx = await edit_or_reply(message, "`Processing...`")
+    try:
+        await xxnx.edit(f"**Berhasil Bergabung ke Chat ID** `{panda}`")
+        await client.join_chat(panda)
+    except Exception as ex:
+        await xxnx.edit(f"**ERROR:** \n\n{str(ex)}")
+
+
+@ilhammansiz_on_cmd(
+    ["leavegrup", "kickme"],
+    cmd_help={
+        "help": "Untuk keluar ke obrolan gc",
+        "example": "{ch}kickme ",
+    },
+)
+async def leave(client, message):
+    panda = message.command[1] if len(message.command) > 1 else message.chat.id
+    xxnx = await edit_or_reply(message, "`Processing...`")
+    try:
+        await xxnx.edit_text(f"{client.me} has left this group, bye!!")
+        await client.leave_chat(panda)
+    except Exception as ex:
+        await xxnx.edit_text(f"**ERROR:** \n\n{str(ex)}")
+
+
+@ilhammansiz_on_cmd(
+    ["leaveall"],
+    cmd_help={
+        "help": "keluar dari semua grup",
+        "example": "{ch}leaveall",
+    },
+)
+async def kickmeall(client, message):
+    panda = await edit_or_reply(message, "`Global Leave from group chats...`")
+    er = 0
+    done = 0
+    async for dialog in client.iter_dialogs():
+        if dialog.chat.type in ("group", "supergroup"):
+            chat = dialog.chat.id
+            try:
+                done += 1
+                await client.leave_chat(chat)
+            except BaseException:
+                er += 1
+    await panda.edit(
+        f"**Berhasil Keluar dari {done} Group, Gagal Keluar dari {er} Group**"
+    )
