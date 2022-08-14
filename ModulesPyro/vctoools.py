13b8f58a5f9bf5b1c7313733687a5d6a6f9ed11b
ilham77mansiz <imansiez7@gmail.com>
2022-08-14 10:10:02 +0000

    PandaX_Userbot: auto fixes
    Signed-off-by: ilham77mansiz <imansiez7@gmail.com>

--- ModulesPyro/vctoools.py
+++ ModulesPyro/vctoools.py
@@ -15,10 +15,6 @@ from Panda import DEVLIST as DEVS, Pyrogrambot
 from Panda._func.decorators import Panda_cmd as ilhammansiz_on_cmd
 from Panda._func._helpers import (
     edit_or_reply,
-    edit_or_send_as_file,
-    get_text,
-    get_user,
-    is_admin_or_owner,
 )
 
 from . import HELP
