e190ef3a77207ae50eb71fa73e7da866b7f5aa91
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-08-14 19:55:13 +0700

    Update clone.py

--- ModulesPyro/clone.py
+++ ModulesPyro/clone.py
@@ -22,7 +22,7 @@ if not hasattr(STORAGE, "userObj"):
 )
 async def impostor(client, message):
     inputArgs = message.text
-    xx = await edit_or_reply(event, "`Processing...`")
+    xx = await edit_or_reply(message, "`Processing...`")
     if "restore" in inputArgs:
         await message.edit("**Kembali ke identitas asli...**")
         if not STORAGE.userObj:
