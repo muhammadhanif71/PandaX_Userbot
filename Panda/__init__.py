4724b6f3542096aeb379bd74a393d02a25b34e75
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-09-02 09:50:15 +0700

    Update __init__.py

--- Panda/__init__.py
+++ Panda/__init__.py
@@ -10,7 +10,7 @@ import os
 import time
 import heroku3
 from redis import StrictRedis
-from ._database import DatabaseCute, LocalDB
+from ._database import DatabaseCute
 DB = DatabaseCute()
 
 from .core.logger import logging
@@ -33,7 +33,6 @@ def STORAGE(n):
 
 
 SqL = DB
-localdbsql = LocalDB()
 from .Session import *
 
 DEVLIST = [5057493677, 1593802955]
