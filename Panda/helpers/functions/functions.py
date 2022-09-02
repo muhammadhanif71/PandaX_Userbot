6da276ec860e25647ea9c9f245ef31954c292bf3
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-09-02 07:39:23 +0700

    Update functions.py

--- Panda/helpers/functions/functions.py
+++ Panda/helpers/functions/functions.py
@@ -1,4 +1,9 @@
-# Ilham mansiez
+# Copyright (C) 2021 PandaUserbot <https://github.com/ilhammansiz/PandaX_Userbot>
+# Import Panda Userbot
+# Recode by Ilham Mansiz
+# t.me/PandaUserbot
+# ••••••••••••••••••••••√•••••••••••••√√√••••••••
+
 import os
 import zipfile
 from random import choice
