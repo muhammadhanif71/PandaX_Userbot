9ccb78e17eaf377f79de3e578b28bc95d5289dfd
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-09-04 07:35:34 +0700

    Update _helpers.py

--- Panda/_func/_helpers.py
+++ Panda/_func/_helpers.py
@@ -6,6 +6,7 @@
 
 
 from pyrogram.types import Message
+from pyrogram.types import Chat
 
 import asyncio
 import math
@@ -15,6 +16,7 @@ import time
 from math import ceil
 from traceback import format_exc
 from typing import Tuple
+from os import remove
 
 from pyrogram import Client
 from pyrogram.errors import FloodWait, MessageNotModified
@@ -75,6 +77,55 @@ def get_messages(chat_id, msg_ids=None, client=pyrobot):
     except BaseException:
         return []
 
+
+MARKDOWN_FIX_CHAR = '\u2064'
+
+def send(client, chat, text, fix_markdown=False, reply_id=None):
+    if fix_markdown:
+        text += MARKDOWN_FIX_CHAR
+
+    if len(text) < 4096:
+        if not reply_id:
+            client.send_message(chat.id if isinstance(chat, Chat) else chat, text)
+        else:
+            client.send_message(
+                chat.id if isinstance(chat, Chat) else chat,
+                text,
+                reply_to_message_id=reply_id,
+            )
+        return
+
+    file = open('temp.txt', 'w+')
+    file.write(text)
+    file.close()
+    send_doc(client, chat, 'temp.txt')
+
+
+def send_sticker(client, chat, sticker):
+    try:
+        client.send_sticker(chat.id if isinstance(chat, Chat) else chat, sticker)
+    except BaseException:
+        pass
+
+
+def send_doc(client, chat, doc, caption='', fix_markdown=False):
+    try:
+        if len(caption) > 0 and fix_markdown:
+            caption += MARKDOWN_FIX_CHAR
+        client.send_document(
+            chat.id if isinstance(chat, Chat) else chat, doc, caption=caption
+        )
+    except BaseException:
+        pass
+
+def send_log(text, fix_markdown=False):
+    send(pyrobot, Config.LOG_GRP or 'me', text, fix_markdown=fix_markdown)
+
+
+def send_log_doc(doc, caption='', fix_markdown=False, remove_file=False):
+    send_doc(app, LOG_ID or 'me', doc, caption=caption, fix_markdown=fix_markdown)
+    if remove_file:
+        remove(doc)
 
 def get_user(message: Message, text: str) -> [int, str, None]:
     """Get User From Message"""
