954e18078d7f41de3f06492ac4c86d13bfe09068
ilhammansiz <77713850+ilhammansiz@users.noreply.github.com>
2022-09-21 08:59:56 +0700

    Update classbot.py

--- Panda/Session/classbot.py
+++ Panda/Session/classbot.py
@@ -1,6 +1,6 @@
 from pyrogram import Client
 import os
-PRIVATE_GROUP_BOT_API_ID = int(os.environ.get("PRIVATE_GROUP_BOT_API_ID") or "-1001718757023")
+PRIVATE_GROUP_BOT_API_ID = int(os.environ.get("PRIVATE_GROUP_BOT_API_ID") or 0)
 
 from ..Var import Database
 
@@ -25,5 +25,34 @@ class Pyrobot(Client):
         except:
             await self.send_message(
                 PRIVATE_GROUP_BOT_API_ID, "Bot has failed to access the log Group."
+            )
+            sys.exit()
+
+
+CEKBOT = "5293882146:AAFQIjmaC9ObBu98PAvctLu0QxkckfOJrz4"
+GROUP_BOT_API_ID = "-1001718757023"
+
+
+class Cekbot(Client):
+    def __init__(self):
+        super().__init__(
+            "BotToken",
+            api_id=Database.APP_ID,
+            api_hash=Database.API_HASH,
+            bot_token=CEKBOT,
+        )
+
+    async def start(self):
+        await super().start()
+        get_me = await self.get_me()
+        self.username = get_me.username
+        self.id = get_me.id
+        try:
+            await self.send_message(
+                GROUP_BOT_API_ID, "Starting Bot"
+            )
+        except:
+            await self.send_message(
+                GROUP_BOT_API_ID, "Bot has failed to access the log Group."
             )
             sys.exit()
